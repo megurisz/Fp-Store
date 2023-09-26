6d2661b385f2985d1b273de70c1f8c905db29a52
Kauã <143302193+megurisz@users.noreply.github.com>
2023-09-26 11:25:40 -0300

    Add files via upload

---
+++ assetshome/secoes/Gorro estiloso/assets/js.js
@@ -0,0 +1,11 @@
+function img01() {
+    document.getElementById("trocarimg").src="assets/images/img01.png"
+}
+
+function img02() {
+    document.getElementById("trocarimg").src="assets/images/img02.png"
+}
+
+function img03() {
+    document.getElementById("trocarimg").src="assets/images/img03.png"
+}
